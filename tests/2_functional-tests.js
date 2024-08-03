78381110c097c79468ec41a97e516e070097fca0
Anthony Burns <anthonyalanb@hotmail.com>
2024-08-03 01:29:59 +0000

    try this

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -107,7 +107,9 @@ suite('Functional Tests with Zombie.js', function () {
         browser.assert.success();
         browser.assert.text('span#name', 'Cristoforo');
         browser.assert.text('span#surname', 'Colombo');
-        browser.assert.elements('span#dates', 1);    
+        browser.assert.elements('span#dates', 1);
+        browser.assert.element('span#dates', 1);
+
         done(); 
       });
     });

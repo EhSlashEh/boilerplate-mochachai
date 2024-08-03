799b49f679f53e5ae49cee1482c04eb3e623a979
Anthony Burns <anthonyalanb@hotmail.com>
2024-08-03 01:18:56 +0000

    asdasd

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -103,9 +103,19 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.equal();
-
-      done();
+      browser.fill('surname', 'Colombo').pressButton('submit', function() {
+   
+        // assert that status is OK 200
+        browser.assert.success();
+        // assert that the text inside the element 'span#name' is 'Cristoforo'
+        browser.assert.text('span#name', 'Cristoforo');
+        // assert that the text inside the element 'span#surname' is 'Colombo'
+        browser.assert.text('span#surname', 'Colombo');
+        // assert that the element(s) 'span#dates' exist and their count is 1
+        browser.assert.element('span#dates', 1);
+    
+        done(); // It's an async test, so we have to call 'done()''
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {

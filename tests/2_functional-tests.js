4d897d92777f34223090d2d7ee5e0a98f334a140
Anthony Burns <anthonyalanb@hotmail.com>
2024-08-03 01:42:16 +0000

    try force

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -104,11 +104,11 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
+      assert.equal();
       browser.fill('surname', 'Colombo').pressButton('submit', function() {   
         browser.assert.success();
         browser.assert.text('span#name', 'Cristoforo');
         browser.assert.text('span#surname', 'Colombo');
-        // browser.assert.element('span#dates', 1);
         browser.assert.elements('span#dates', 1);
         done(); 
       });
